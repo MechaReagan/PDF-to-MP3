4e9702feec0de57258bc885b749986e92a10cd56
MechaReagan <113811371+MechaReagan@users.noreply.github.com>
2022-11-10 14:16:09 -0600

    Add files via upload

--- main.py
+++ main.py
@@ -1,42 +1,34 @@
-import tkinter as tk
-import random
-
-window = tk.Tk()
-window.title("Disappearing Text Writing App")
-window.config(background="#ADD8E6")
-window.geometry("600x800")
-
-prompts = ["Set your story in an oracle or a fortune teller’s parlor.",
-           "Set your story in a world where the currency isn’t money — or at least not money as we understand it.",
-           "Start your story with someone emerging from the sea, and end it with them looking up at the stars.",
-           "Write about a character who’s had their future foretold from birth — but isn’t sure if they believe it.",
-           "One day, the sun rose in the west and set in the east."]
-chosen_prompt = random.choice(prompts)
-
-prompt_frame = tk.Frame(height=200, width=550)
-prompt_frame.place(x=300, y=150, anchor="center")
-
-prompt = tk.Label(prompt_frame, text=chosen_prompt, wraplength=500, font=("Arial", 14))
-prompt.place(x=275, y=100, anchor="center")
-
-writing_box = tk.Text(height=25, width=60, wrap="word" )
-writing_box.place(x=300, y=550, anchor="center")
-writing_box.focus()
-entry = writing_box.get("1.0", "end-1c")
+from gtts import gTTS
+import PyPDF2
 
 
-def check_progress():
-    global entry
-    if entry != writing_box.get("1.0", "end-1c"):
-        entry = writing_box.get("1.0", "end-1c")
+file_path = input("What is the file path of the PDF you would like to convert?\n")
+mp3_name = input("What would you like to name this mp3 file?\n")
+
+
+text = []
+try:
+    with open(f"{file_path}", mode='rb') as f:
+        reader = PyPDF2.PdfFileReader(f)
+        for page in range(reader.getNumPages()):
+            read_page = reader.getPage(page)
+            page_text = read_page.extractText()
+            text.append(page_text)
+
+    text_body = "".join(text)
+except FileNotFoundError:
+    print("I'm sorry, but I didn't recognize that file path. Please double check and try again.")
+
+else:
+    print("\nYour PDF is being converted to an mp3. \nPlease wait a moment...")
+    language = 'en'
+
+    myobj = gTTS(text=text_body, lang=language, slow=False)
+
+    if ".mp3" in mp3_name:
+        mp3_name = mp3_name.replace(".mp3", "")
+        myobj.save(f"{mp3_name}.mp3")
     else:
-        writing_box.delete('1.0', "end")
-    window.after(6000, check_progress)
+        myobj.save(f"{mp3_name}.mp3")
 
-
-window.update()
-check_progress()
-
-window.mainloop()
-
-
+    print("\nYour PDF has been converted and saved in this directory. Have a wonderful day!")
